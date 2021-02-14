7e04a7d931a85b14ac6e0a380fcc18ef24426495
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-14 13:54:19 +0530

    update

--- include/initialize.php
+++ include/initialize.php
@@ -14,7 +14,7 @@
 //(\ for windows, / for Unix)
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'test/pems');
+defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'test/project-expense-monitoring');
 
 defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'include');
 
